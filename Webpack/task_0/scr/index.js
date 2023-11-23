f0a482e30de39aced70eac488c8b19cf16b8b20e
SiwarAyari <siwarayari1999@gmail.com>
2023-11-23 11:55:52 +0000

    creat and run webpack

--- Webpack/task_0/scr/index.js
+++ Webpack/task_0/scr/index.js
@@ -3,5 +3,3 @@ import $ from "jquery";
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<p>Copyright - Holberton School</p>');import $ from "jquery";
-
-
