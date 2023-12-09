b72123d3265b9d24749c25935784e5ef8d56039d
AyariSiwar <91055926+Siwarayari@users.noreply.github.com>
2023-12-09 21:07:13 +0100

    task4

---
+++ react_props/task_4/dashboard/src/Notifications/Notifications.test.js
@@ -0,0 +1,39 @@
+import { shallow } from 'enzyme';
+import Notification from "./Notifications";
+
+describe('<MyComponent />', () => {
+  it('renders app', () => {
+    const wrapper = shallow(<Notification />);
+    expect(wrapper.exists);
+  });
+
+  it('renders App-header', () => {
+    const wrapper = shallow(<Notification />);
+    expect(wrapper.find('NotificationItem')).to.have.lengthOf(3);
+  });
+
+  it('renders App-body', () => {
+    const { getByText } = within(screen.getByTestId('Notifications'))
+    expect(getByText('Here is the list of notifications')).toBeInTheDocument()
+  });
+
+  it('renders App-footer', () => {
+    const wrapper = shallow(<Notification displayDrawer/>);
+    expect(wrapper.find("menuItem")).to.have.lengthOf(0);
+  });
+
+  it('renders App-footer', () => {
+    const wrapper = shallow(<Notification/>);
+    expect(wrapper.find("Notifications")).to.have.lengthOf(1);
+  });
+
+  it('renders App-footer', () => {
+    const wrapper = shallow(<Notification/>);
+    expect(wrapper.find("menuItem")).to.have.lengthOf(0);
+  });
+
+  it('renders App-footer', () => {
+    const wrapper = shallow(<Notification displayDrawer/>);
+    expect(wrapper.find("Notifications")).to.have.lengthOf(1);
+  });
+});
