1298886310524dddd2d5cda7e6497a376741c0fe
AyariSiwar <91055926+Siwarayari@users.noreply.github.com>
2023-12-11 15:15:39 +0100

    task5

---
+++ react_props/task_5/dashboard/src/App/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import { getFullYear,getFooterCopy } from './utils';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
+import Notifications from '../Notifications/Notifications';
+import PropTypes from 'prop-types';
+import CourseList from '../CourseList/CourseList';
+
+function App() {
+  return (
+    <React.Fragment>
+    <Notifications listNotifications={listNotifications}/>
+    <div className="App">
+    <Header />
+    <div className="App-body">
+    {isLoggedIn ? (
+        <CourseList />
+      ) : (
+        <Login />
+      )}
+    </div>
+    <div className="App-footer">
+    <Footer />
+    </div>
+    </div>
+    </React.Fragment>
+  );
+}
+
+App.defaultProps = {
+  isLoggedIn: false
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
+
+const listCourses = [
+  {id: 1, name: 'ES6', credit: 60},
+  {id: 2, name: 'Webpack', credit: 20},
+  {id: 3, name: 'React', credit: 40}
+];
+
+export default App;
