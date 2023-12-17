333078eee8b7c14b2086457f08188a5494252dfd
AyariSiwar <91055926+Siwarayari@users.noreply.github.com>
2023-12-17 13:24:24 +0100

    task1

---
+++ React_inline_styling/task_1/dashboard/src/Footer/Footer.js
@@ -0,0 +1,12 @@
+import './footer.css';
+import { getFullYear,getFooterCopy } from './utils';
+
+function Footer() {
+  return (
+      <footer className="App-footer">
+      <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+  );
+}
+
+export default Footer;
