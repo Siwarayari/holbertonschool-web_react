3275442f4851341a3398ce942df353ab745f6e59
AyariSiwar <91055926+Siwarayari@users.noreply.github.com>
2023-12-17 13:36:53 +0100

    task1

---
+++ React_inline_styling/task_1/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { getLatestNotification } from './utils';
+import NotificationItem from './NotificationItem';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+    bodySectionWithMargin: {
+        marginBottom: '40px'
+  }
+  })
+
+
+
+class Notifications extends React.Component {
+ 
+  constructor(props) {
+      super(props)
+      this.state = {
+          html: PropTypes.string,
+          value: PropTypes.string,
+          type: PropTypes.string.isRequired
+      }
+  }
+
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+}
+
+shouldComponentUpdate(nextProps) {
+  if (nextProps.listNotifications.length === this.props.listNotifications.length) {
+      return false
+  }
+  return true
+}
+
+
+  render() {
+    return (
+      <><div className="menuItem">
+        <p>Your notifications</p>
+      </div>
+      {displayDrawer ? (
+              <div className="Notifications">
+              <p>Here is the list of notifications</p>
+              <button aria-label='Close' onClick={buttonclc()} style={btncs}>
+                <img src='close-icon'></img>
+              </button>
+              <li markAsRead={this.markAsRead} onClick="markAsRead()"></li>
+              <ul>
+              {listCourses ? (
+                  <tr>No new notification for now</tr>
+                ) : (
+                  listNotifications.map(listNotification => (
+                      <NotificationItem type={listNotification.type} value={listNotification.value} dangerouslySetInnerHTML={listNotification.__html}/>
+                ))
+                )}
+              </ul>
+            </div>
+      ) : (
+        <Login />
+      )}
+</>
+    );
+  }
+}
+
+  function buttonclc(){
+    console.log("Close button has been clicked");
+}
+const btncs = {
+    position: 'fixed',
+    float: 'right',
+    background: 'transparent',
+	  color: 'white',
+    top: '10px',
+    right: '10px',
+    height: '17px'
+}
+
+Notifications.PropTypes = {
+  displayDrawer: PropTypes.bool,
+}
+Notifications.defaultProps = {
+  displayDrawer: false,
+}
+export default Notifications;
