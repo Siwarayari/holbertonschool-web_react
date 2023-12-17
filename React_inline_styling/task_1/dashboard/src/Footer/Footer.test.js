54f870631d55d6a9b717fd59de07a19248741dda
AyariSiwar <91055926+Siwarayari@users.noreply.github.com>
2023-12-17 13:25:00 +0100

    task1

---
+++ React_inline_styling/task_1/dashboard/src/Footer/Footer.test.js
@@ -0,0 +1,13 @@
+import { shallow } from 'enzyme';
+import Footer from './Footer';
+
+describe('Footer', () => {
+  it('verify it renders without crashing', () => {
+    shallow(<Footer />);
+  });
+
+  it('img', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find('Footer')).toContain('Copyright')
+  });
+});
