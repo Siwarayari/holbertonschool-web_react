8037c699152fe7205abb30ddaa6f168e0ea558a0
AyariSiwar <91055926+Siwarayari@users.noreply.github.com>
2024-01-08 19:22:53 +0100

    CourseListRow.js

---
+++ 0x08_react_redux_action_creator_normalizr/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,45 @@
+import { StyleSheet, css } from 'aphrodite';
+import React, { useState } from 'react';
+
+const backgroundColorth = { backgroundColor: '#deb5b545'};
+const backgroundColortr = { backgroundColor: '#f5f5f5ab'};
+
+const styles = StyleSheet.create({
+    bodySectionWithMargin: {
+        marginBottom: '40px'
+  },
+  rowChecked: {
+    backgroundColor: '#e6e4e4'
+  }
+  })
+
+function CourseListRow({isHeader, textFirstCell, textSecondCell}){
+        if (isHeader) {
+            if (textFirstCell === null){
+                return (
+                    <tr style={backgroundColortr}>
+                        <th colSpan = {2} style={backgroundColorth}>{textFirstCell}</th>
+                    </tr>
+                )
+            }
+            else {
+                return (
+                    <tr style={backgroundColortr}>
+                        <th style={rowChecked} type="checkbox">{textFirstCell}</th>
+                        <th style={backgroundColorth}>{textSecondCell}</th>
+                    </tr>
+                )
+            }
+        }
+        else {
+            <tr style={backgroundColortr}>
+                <th style={backgroundColorth}>{textFirstCell}</th>
+                <th style={backgroundColorth}>{textSecondCell}</th>
+            </tr>
+        }
+}
+
+CourseListRow.propTypes = {
+    textSecondCell : oneOfType([string, number]),
+  };
+export default CourseListRow;
